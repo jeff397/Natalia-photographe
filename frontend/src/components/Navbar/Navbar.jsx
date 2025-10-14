93203f4cf85eba9aac9e62743c9d9dde8e757978
jeff <jeffdelmotte@gmail.com>
2025-10-14 18:33:27 +0200

    mise en place du menu déroulant

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,12 +5,15 @@ import "./navbar.css";
 
 function Navbar() {
   const { isLoggedIn, logout } = useContext(AuthContext);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // menu mobile
+  const [offersOpen, setOffersOpen] = useState(false); // dropdown mobile
 
   const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleOffers = () => setOffersOpen(!offersOpen);
 
   return (
     <nav className="nav">
+      {/* Menu gauche desktop */}
       <div className="nav-left">
         <Link to="/" className="menu-link">
           Accueil
@@ -20,17 +23,39 @@ function Navbar() {
         </Link>
       </div>
 
+      {/* Logo */}
       <Link to="/" className="logo-container">
         <h1 className="logo">Natalia GoJa</h1>
       </Link>
 
+      {/* Menu droit desktop */}
       <div className="nav-right">
-        <Link to="/MyOffers" className="menu-link">
-          Tarifs
-        </Link>
+        {/* Offres desktop */}
+        <div
+          className={`offers-container ${offersOpen ? "active" : ""}`}
+          onMouseEnter={() => setOffersOpen(true)}
+          onMouseLeave={() => setOffersOpen(false)}
+        >
+          <div className="menu-link offers-link" onClick={toggleOffers}>
+            Offres
+          </div>
+          <div className="dropdown">
+            <Link to="/photo-events" className="dropdown-link">
+              Célébrations familiales & amicales
+            </Link>
+            <Link to="/portraits" className="dropdown-link">
+              Portraits & sessions privées
+            </Link>
+            <Link to="/reporting" className="dropdown-link">
+              Reportages
+            </Link>
+          </div>
+        </div>
+
         <Link to="/Contact" className="menu-link">
           Contact
         </Link>
+
         {isLoggedIn ? (
           <span className="menu-link" onClick={logout}>
             Logout
@@ -54,13 +79,37 @@ function Navbar() {
         >
           Portfolio
         </Link>
-        <Link
-          to="/Myoffers"
-          className="menu-link"
-          onClick={() => setIsOpen(false)}
-        >
-          Tarifs
-        </Link>
+
+        {/* Offres mobile */}
+        <div className="menu-link offers-link" onClick={toggleOffers}>
+          Offres
+          {offersOpen && (
+            <div className="dropdown">
+              <Link
+                to="/photo-events"
+                className="dropdown-link"
+                onClick={() => setIsOpen(false)}
+              >
+                Célébrations familiales & amicales
+              </Link>
+              <Link
+                to="/portraits"
+                className="dropdown-link"
+                onClick={() => setIsOpen(false)}
+              >
+                Portraits & sessions privées
+              </Link>
+              <Link
+                to="/reporting"
+                className="dropdown-link"
+                onClick={() => setIsOpen(false)}
+              >
+                Reportages
+              </Link>
+            </div>
+          )}
+        </div>
+
         <Link
           to="/Contact"
           className="menu-link"
@@ -68,6 +117,7 @@ function Navbar() {
         >
           Contact
         </Link>
+
         {isLoggedIn ? (
           <span
             className="menu-link"
@@ -89,6 +139,7 @@ function Navbar() {
         )}
       </div>
 
+      {/* Burger */}
       <div className="burger" onClick={toggleMenu}>
         <div className="line"></div>
         <div className="line"></div>
