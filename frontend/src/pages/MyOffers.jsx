5a2ffe4829e4b6278da6386e9e0fc72baa0481f3
jeff <jeffdelmotte@gmail.com>
2025-10-14 18:37:04 +0200

    suppression de la page Myoffers

--- frontend/src/pages/MyOffers.jsx
+++
@@ -1,9 +0,0 @@
-import Pricing from "../components/Pricing/Pricing"; // ajuster le chemin selon ton projet
-
-export default function MyOffers() {
-  return (
-    <div>
-      <Pricing />
-    </div>
-  );
-}
