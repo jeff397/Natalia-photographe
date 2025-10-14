713dc651452b967b4354d494b3a5f2abc9f4cb6b
jeff <jeffdelmotte@gmail.com>
2025-10-14 18:31:33 +0200

    mise en place des icones sociaux

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaInstagram, FaLinkedin, FaFacebookF } from "react-icons/fa"; // import icônes
 import "./footer.css";
 
 function Footer() {
@@ -7,25 +8,39 @@ function Footer() {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-section">
-          <a href="#">Accueil</a>
-          <a href="#">À propos</a>
-          <a href="#">Contact</a>
+          <Link to="/">Accueil</Link>
+          <Link to="/offers">Offres</Link>
+          <Link to="/contact">Contact</Link>
         </div>
 
         <div className="social-icons">
-          <a href="#" aria-label="Facebook">
-            F
+          <a
+            href="https://www.instagram.com/natalia.goja.photo/ "
+            aria-label="Instagram"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
           </a>
-          <a href="#" aria-label="Instagram">
-            I
-          </a>
-          <a href="#" aria-label="LinkedIn">
-            L
+          <a
+            href="https://www.linkedin.com/in/natalia-godon-jamio%C5%82kowska/ "
+            aria-label="LinkedIn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
           </a>
         </div>
 
         <div className="footer-bottom">
-          &copy; 2025 MonSite. Tous droits réservés.
+          &copy; 2025 -{" "}
+          <a
+            href="https://portfolio-8awh.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            site créé par Delmotte Jean-François
+          </a>
         </div>
       </div>
     </footer>
